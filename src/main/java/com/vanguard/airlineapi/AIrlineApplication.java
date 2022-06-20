4564b35b5589f33fc9c8e39867c2964fe7ba9ed5
jbrennan7651 <103645942+jbrennan7651@users.noreply.github.com>
2022-06-20 11:20:13 -0400

    initial commit

---
+++ src/main/java/com/vanguard/airlineapi/AIrlineApplication.java
@@ -0,0 +1,13 @@
+package com.vanguard.airlineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIrlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIrlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanguard/airlineapi/AIrlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.airlineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIrlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
